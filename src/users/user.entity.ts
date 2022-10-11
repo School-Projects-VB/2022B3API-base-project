3708ce9337601a6213d0d3617b0be1bf4fcf8a38
VBetsch <vic.betsch@gmail.com>
2022-10-11 15:59:02 +0200

    ✨ User constructor class created

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,9 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export class User {
-  public id!: uuidv4;
-  public username!: string; // cette propriété doit porter une contrainte d'unicité
-  public email!: string; // cette propriété doit porter une contrainte d'unicité
+  
+  constructor(
+      name: string, 
+      mail: string, 
+      pwd: string, 
+      type: 'Employee' | 'Admin' | 'ProjectManager'
+    ){
+    this.id = uuidv4(); // TODO: Format uuidv4
+    this.username = name;
+    this.email = mail;
+    this.password = pwd;
+    this.role = type;
+  }
+
+  public id!: string; 
+  public username!: string; // TODO: Cette propriété doit porter une contrainte d'unicité
+  public email!: string; // TODO: Cette propriété doit porter une contrainte d'unicité
   public password!: string;
-  public role!: 'Employee' | 'Admin' | 'ProjectManager' // valeur par defaut : 'Employee'
+  public role!: 'Employee' | 'Admin' | 'ProjectManager' // TODO: Valeur par defaut 'Employee'
 }
