6decc78c1cd48ae80f730dd99a7308beb82a6fd0
VBetsch <vic.betsch@gmail.com>
2022-10-11 16:09:06 +0200

    ✨ Set role default value

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,22 +2,21 @@ import { v4 as uuidv4 } from 'uuid';
 
 export class User {
   
-  constructor(
-      name: string, 
-      mail: string, 
-      pwd: string, 
-      type: 'Employee' | 'Admin' | 'ProjectManager'
-    ){
-    this.id = uuidv4(); // TODO: Format uuidv4
-    this.username = name;
-    this.email = mail;
+  constructor(name: string, 
+              mail: string, 
+              pwd: string, 
+              type: 'Employee' | 'Admin' | 'ProjectManager' = 'Employee')
+  {
+    this.id = uuidv4();
+    this.username = name; // TODO: Cette propriété doit porter une contrainte d'unicité
+    this.email = mail; // TODO: Cette propriété doit porter une contrainte d'unicité
     this.password = pwd;
-    this.role = type;
+    this.role = type;// TODO: Valeur par defaut 'Employee'
   }
 
   public id!: string; 
-  public username!: string; // TODO: Cette propriété doit porter une contrainte d'unicité
-  public email!: string; // TODO: Cette propriété doit porter une contrainte d'unicité
+  public username!: string;
+  public email!: string;
   public password!: string;
-  public role!: 'Employee' | 'Admin' | 'ProjectManager' // TODO: Valeur par defaut 'Employee'
+  public role!: 'Employee' | 'Admin' | 'ProjectManager' 
 }
