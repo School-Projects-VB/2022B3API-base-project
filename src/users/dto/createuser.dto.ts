57ffff6458ad02905a445c21e745fcb35b9aa533
VBetsch <vic.betsch@gmail.com>
2022-10-11 16:47:02 +0200

    ✨ Password added to create user dto

--- src/users/dto/createuser.dto.ts
+++ src/users/dto/createuser.dto.ts
@@ -1,7 +1,13 @@
 import { IsEmail, IsNotEmpty } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  password: string;
 }
