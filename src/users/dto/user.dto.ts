56e6e97b0d59d0d1a4281cb7dfab305497d6a4c1
VBetsch <vic.betsch@gmail.com>
2022-10-17 14:55:19 +0200

    ✨ Add `ApiProperty`

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -4,17 +4,21 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsNotEmpty()
   @MinLength(3)
+  @ApiProperty()
   username: string;
 
   @IsNotEmpty()
   @MinLength(8)
+  @ApiProperty()
   password: string;
 
   @IsOptional()
+  @ApiProperty()
   @IsIn(['Employee', 'Admin', 'ProjectManager'])
   role: 'Employee' | 'Admin' | 'ProjectManager';
 }
