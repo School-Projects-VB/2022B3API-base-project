86d3ccae6e311c58303e3f15895ee83a223d611e
VBetsch <vic.betsch@gmail.com>
2022-10-18 15:13:20 +0200

    🔥 Clear bad entities

--- src/project-users/project-user.entity.ts
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-
-class ProjectUser {
-  public id!: uuidv4;
-  public startDate!: Date;
-  public endDate!: Date;
-  public projectId!: uuidv4;
-  public userId!: uuidv4;
-}
--- src/projects/project.entity.ts
+++
@@ -1,7 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-
-class Project {
-  public id!: uuidv4;
-  public name!: string;
-  public referringEmployeeId!: uuidv4;
-}
