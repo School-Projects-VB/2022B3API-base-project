f4bd607527bcf025cc0f3585269e8e11d8b248df
VBetsch <vic.betsch@gmail.com>
2022-11-22 15:17:36 +0100

    :sparkles: Add description

--- src/projects/dto/createproject.dto.ts
+++ src/projects/dto/createproject.dto.ts
@@ -1,10 +1,15 @@
 import {IsNotEmpty} from "class-validator";
 import { ApiProperty } from '@nestjs/swagger';
+import {Optional} from "@nestjs/common";
 
 export class CreateProjectDto {
   @IsNotEmpty()
   @ApiProperty()
   name: string;
+
+  @Optional()
+  @ApiProperty()
+  description: string;
 
   @IsNotEmpty()
   @ApiProperty()
